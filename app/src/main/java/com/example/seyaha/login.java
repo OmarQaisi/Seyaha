5d1bf9c342fdde28a00a2e9f4f30c9770a59bc57
Omar Qaisi <thesaver23@gmail.com>
2019-07-20 23:38:02 +0300

    initial commit

---
+++ app/src/main/java/com/example/seyaha/login.java
@@ -0,0 +1,14 @@
+package com.example.seyaha;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
