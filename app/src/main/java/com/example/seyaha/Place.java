562716a4688db9e66950ddf1e55b2ec2145be3be
Omar Qaisi <thesaver23@gmail.com>
2019-08-20 17:01:19 +0300

    added montserrat font and touched on Place class

--- app/src/main/java/com/example/seyaha/Place.java
+++ app/src/main/java/com/example/seyaha/Place.java
@@ -1,37 +1,40 @@
 package com.example.seyaha;
 
 public class Place {
-    public String nameAR;
-    public String nameEN;
-    public String descAR;
-    public String descEN;
+
+
     public String categoryAR;
     public String categoryEN;
-    public String coordinates;
-    public String imageURL;
     public int cost;
+    public String descAR;
+    public String descEN;
+    public int estimatedTime;
+    public String imageURL;
     public int internet;
-    public int airQuality;
-    public int recommendedEstimation;
+    public double latitude;
+    public double longitude;
+    public String nameAR;
+    public String nameEN;
+    public int recommendedAge;
     public int recommendedSeason;
     public int recommendedTime;
-    public int recommendedAge;
 
-    public Place(String nameAR, String nameEN, String descAR, String descEN, String categoryAR, String categoryEN, String coordinates, String imageURL, int cost, int internet, int airQuality, int recommendedEstimation, int recommendedSeason, int recommendedTime, int recommendedAge) {
-        this.nameAR = nameAR;
-        this.nameEN = nameEN;
-        this.descAR = descAR;
-        this.descEN = descEN;
+
+    public Place(String categoryAR, String categoryEN, int cost, String descAR, String descEN, int estimatedTime, String imageURL, int internet, double latitude, double longitude, String nameAR, String nameEN, int recommendedAge, int recommendedSeason, int recommendedTime) {
         this.categoryAR = categoryAR;
         this.categoryEN = categoryEN;
-        this.coordinates = coordinates;
-        this.imageURL = imageURL;
         this.cost = cost;
+        this.descAR = descAR;
+        this.descEN = descEN;
+        this.estimatedTime = estimatedTime;
+        this.imageURL = imageURL;
         this.internet = internet;
-        this.airQuality = airQuality;
-        this.recommendedEstimation = recommendedEstimation;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.nameAR = nameAR;
+        this.nameEN = nameEN;
+        this.recommendedAge = recommendedAge;
         this.recommendedSeason = recommendedSeason;
         this.recommendedTime = recommendedTime;
-        this.recommendedAge = recommendedAge;
     }
 }
