f66b63c797dd1cb9a871af10a8fd1511ac03a496
anas <anasrasheed64@gmail.com>
2019-09-09 01:16:22 +0300

    fix add tour code

--- app/src/main/java/com/example/seyaha/Cost.java
+++ app/src/main/java/com/example/seyaha/Cost.java
@@ -1,16 +1,18 @@
 package com.example.seyaha;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class Cost
+public class Cost implements Serializable
 {
 
-int activities;
-int entranceFees;
-int food;
-int transportation;
-List<SleepingPlace> overNightStay;
+public int activities;
+public int entranceFees;
+public int food;
+public int transportation;
+public List<SleepingPlace> overNightStay;
 
+public Cost(){}
     public Cost(int activities, int entranceFees, int food, int transportation, List<SleepingPlace> overNightStay) {
         this.activities = activities;
         this.entranceFees = entranceFees;
--- app/src/main/java/com/example/seyaha/SleepingPlace.java
+++ app/src/main/java/com/example/seyaha/SleepingPlace.java
@@ -1,11 +1,14 @@
 package com.example.seyaha;
 
-public class SleepingPlace
+import java.io.Serializable;
+
+public class SleepingPlace implements Serializable
 {
 
-    String name;
-    int price;
+  public   String name;
+   public int price;
 
+   public SleepingPlace(){}
     public SleepingPlace(String name, int price) {
         this.name = name;
         this.price = price;
